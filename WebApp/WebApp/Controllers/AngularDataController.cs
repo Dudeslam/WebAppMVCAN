bb246a8e1741ef9f8e5aca264f1fe5d098b6be24
Vinh Trung Thai <vtt@bytelab.dk>
2022-03-22 15:26:12 +0100

    First working example

---
+++ WebApp/WebApp/Controllers/AngularDataController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class AngularDataController : Controller
+    {
+        // GET: AngularData
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
